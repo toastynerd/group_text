96b1a9d9bc4c0ee369bfde0ed760ef5e077b0561
toastynerd <tylermorgan86@gmail.com>
2015-05-12 11:02:15 -0700

    add a welcome page to the actual website

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-var express    = require('express');
+var express = require('express');
+var fs = require('fs');
 var bodyParser = require('body-parser');
-var mongoose   = require('mongoose');
+var mongoose = require('mongoose');
 var textFunctions = require('./lib/textFunctions');
-var app        = express();
+var app = express();
 
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/pax-watch');
 
@@ -17,6 +18,17 @@ app.post('/textIn', function(req, res){
   } else {
     textFunctions.globalSend(req, res, input);
   }
+});
+
+app.get('/style.css', function(req, res) {
+  res.set('Content-Type', 'text/css');
+  var readStream = fs.createReadStream('./static/style.css');
+  readStream.pipe(res);
+});
+
+app.get('*', function(req, res) {
+  var readStream = fs.createReadStream('./static/welcome.html');
+  readStream.pipe(res);
 });
 
 app.set('port', process.env.PORT || 3000);
